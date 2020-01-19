82b8238b4fcfd650c99571442e118eed257e60b4
Yerdos Ordabayev <ordabayev@brandeis.edu>
2020-01-18 20:30:35 -0500

    h,w,x(theta)

--- example/bash.sh
+++ example/bash.sh
@@ -80,7 +80,13 @@
 #python main.py --num-steps=30000 --model=tracker --n-batch=16 --learning-rate=0.0051 --dataset=LarryCy3sigma54Short --device=cuda0
 #python main.py --num-steps=15000 --model=tracker --n-batch=10 --learning-rate=0.005 --dataset=LarryCy3sigma54 --device=cuda0
 #python main.py --num-steps=20000 --model=tracker --n-batch=16 --learning-rate=0.005 --dataset=GraceArticlePol2NegativeControl --device=cuda1
-python main.py --num-steps=5000 --model=tracker --n-batch=32 --learning-rate=0.0045 --dataset=GraceArticlePol2 --negative-control=GraceArticlePol2NegativeControl --device=cuda1 --jit
-#python main.py --num-steps=30000 --model=tracker --n-batch=8 --learning-rate=0.0045 --dataset=LarryCy3sigma54 --negative-control=LarryCy3sigma54NegativeControl --device=cuda0 --jit
+#python main.py --num-steps=5000 --model=tracker --n-batch=32 --learning-rate=0.0045 --dataset=GraceArticlePol2 --negative-control=GraceArticlePol2NegativeControl --device=cuda1 --jit
+#python main.py --num-steps=10000 --model=tracker --n-batch=8 --learning-rate=0.0045 --dataset=LarryCy3sigma54 --negative-control=LarryCy3sigma54NegativeControl --device=cuda0 --jit
 #python main.py --num-steps=5000 --model=tracker --n-batch=32 --learning-rate=0.003 --dataset=FL_1118_2225_0p3OD --device=cuda0
 #python main.py --num-steps=50000 --model=tracker --n-batch=16 --learning-rate=0.003 --dataset=LarryCy3sigma54Short --negative-control=LarryCy3sigma54NegativeControlShort --device=cuda0 --jit
+#python main.py --num-steps=30000 --model=tracker --n-batch=32 --learning-rate=0.0045 --dataset=DanPol2 --negative-control=DanPol2NegativeControl --device=cuda1 --jit
+#python main.py --num-steps=5000 --model=tracker --n-batch=32 --learning-rate=0.0059 --dataset=GraceArticleSpt5 --negative-control=GraceArticleSpt5NegativeControl --device=cuda1
+#python main.py --num-steps=15000 --model=tracker --n-batch=32 --learning-rate=0.006 --dataset=GraceArticlePol2 --negative-control=GraceArticlePol2NegativeControl --device=cuda0
+#python main.py --num-steps=15000 --model=tracker --n-batch=32 --learning-rate=0.005 --dataset=DanZhouPol2 --negative-control=DanZhouPol2NegativeControl --device=cuda0 --jit
+python main.py --num-steps=30000 --model=tracker --n-batch=32 --learning-rate=0.004 --dataset=FL_1_1117_0OD --negative-control=FL_1_1117_0OD.NegativeControl --device=cuda1 --jit
+#python main.py --num-steps=30000 --model=tracker --n-batch=32 --learning-rate=0.0035 --dataset=FL_1118_2225_0p3OD --negative-control=FL_1118_2225_0p3OD.NegativeControl --device=cuda0 --jit
