8071fe88539db957a957cd03ab84692e4bd8cdb9
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-06-01 17:48:56 -0400

    .readthedocs.yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,10 @@ intersphinx_mapping = dict(
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [".ipynb_checkpoints"]
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 
 # This is processed by Jinja2 and inserted before each notebook
