632cd2dbf11db590d85eda789b87013a21352500
Yerdos Ordabayev <ordabayev@brandeis.edu>
2022-03-30 08:20:59 -0400

    constrain click

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
+        "click<=8.0.4",
         "colorama",
         "funsor==0.4.2",
         "future",
